ec4ab13556ccd044df6826f273ebc9346a5292fb
Jan Steinke <jan.steinke@gmail.com>
2024-02-04 15:25:13 +0100

    generate a random simon says sequence

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
-	"syscall/js"
+	"math/big"
 )
 
 func main() {
-	fmt.Println("Hello world!")
-	val := js.Global().Get("html")
-	fmt.Println(val)
-	fmt.Println("End")
+	s := GenerateSequence(6)
+	fmt.Printf("%v\n", s)
+}
+
+func GenerateSequence(l int) []int64 {
+	seq := []int64{}
+	for i := 0; i < l; i++ {
+		n, err := rand.Int(rand.Reader, big.NewInt(4))
+		if err != nil {
+			panic(err)
+		}
+		seq = append(seq, n.Int64())
+
+	}
+	return seq
 }
